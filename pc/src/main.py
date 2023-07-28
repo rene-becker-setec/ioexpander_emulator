c7c1cc996c6450de9f9e338fcef680ce7665d1ae
Rene Becker <rene.becker@setec.com.au>
2023-07-28 15:38:40 +1000

    added erpc file

--- pc/src/main.py
+++ pc/src/main.py
@@ -1,3 +1,19 @@
+"""
+
+"""
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)',
+    level=logging.INFO,
+    handlers=[
+        # logging.FileHandler(f'./{datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")}.log'),
+        logging.StreamHandler()
+    ]
+)
+
+LOGGER = logging.getLogger(__name__)
+
 
 if __name__ == '__main__':
-    print('Starting ...')
+    LOGGER.info('Starting ...')
